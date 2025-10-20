50943078eb358e0493d2741ba26540440972c704
s097090 <s097090@student.tu.kielce.pl>
2025-10-20 10:50:59 +0200

    fullscreen na F

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,4 @@
-#include <allegro5/allegro.h>
+ï»¿#include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <iostream>
 #include <allegro5/allegro_font.h>
@@ -6,17 +6,17 @@
 
 int main() {
     if (!al_init()) {
-        std::cerr << "B³¹d inicjalizacji Allegro!" << std::endl;
+        std::cerr << "BÅ‚Ä…d inicjalizacji Allegro!" << std::endl;
         return -1;
     }
-    al_init_font_addon();     
+    al_init_font_addon();
     al_init_ttf_addon();
     al_init_primitives_addon();
     al_install_mouse();
     al_install_keyboard();
 
     const int WIDTH = 800, HEIGHT = 600;
-    al_set_new_display_flags(ALLEGRO_RESIZABLE); // okno mo¿na zmieniaæ
+    al_set_new_display_flags(ALLEGRO_RESIZABLE); // okno moÅ¼na zmieniaÄ‡
     ALLEGRO_DISPLAY* display = al_create_display(WIDTH, HEIGHT);
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
     ALLEGRO_FONT* Rozdzielczosc = al_load_ttf_font("Arial.ttf", 11, 0);
@@ -26,14 +26,14 @@ int main() {
     al_register_event_source(queue, al_get_keyboard_event_source());
 
 
-    //Wymiary i polo¿enie pocz¹tkowe przycisku do rozdzielczoœci
+    //Wymiary i poloÅ¼enie poczÄ…tkowe przycisku do rozdzielczoÅ›ci
     float WorkspacePlace_w = 500, WorkspacePlace_h = 300;
     float ResolutionButton_w = 100, ResolutionButton_h = 20;
     /*float ResolutionButton_x = (WIDTH / 2.0f) - (ResolutionButton_w / 2.0f);
-    float ResolutionButton_y = (HEIGHT / 2.0f) - (ResolutionButton_h / 2.0f); po³o¿enie na œrodku*/
+    float ResolutionButton_y = (HEIGHT / 2.0f) - (ResolutionButton_h / 2.0f); poÅ‚oÅ¼enie na Å›rodku*/
 
     float ResolutionButton_x = 0;// (ResolutionButton_w / 2.0f);
-    float ResolutionButton_y = 0;// (ResolutionButton_h / 2.0f); //po³o¿enie w lewym górnym rogu
+    float ResolutionButton_y = 0;// (ResolutionButton_h / 2.0f); //poÅ‚oÅ¼enie w lewym gÃ³rnym rogu
 
     float WorkspacePlace_x = (WIDTH / 2.0f) - (WorkspacePlace_w / 2.0f);
     float WorkspacePlace_y = (HEIGHT / 2.0f) - (WorkspacePlace_h / 2.0f);
@@ -44,11 +44,13 @@ int main() {
     int current_w = 800;
     int current_h = 640;
 
+    bool fullscreen = false;
+
     while (running) {
         ALLEGRO_EVENT ev;
         al_wait_for_event(queue, &ev);
 
-        // Zamkniêcie okna
+        // ZamkniÄ™cie okna
         if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
             running = false;
 
@@ -59,7 +61,7 @@ int main() {
             int new_w = al_get_display_width(display);
             int new_h = al_get_display_height(display);
 
-            // ponowne wyœrodkowanie przycisku
+            // ponowne wyÅ›rodkowanie przycisku
             ResolutionButton_x = 0;
             ResolutionButton_y = 0;
 
@@ -69,7 +71,7 @@ int main() {
             WorkspacePlace_y = (new_h / 2.0f) - (WorkspacePlace_h / 2.0f);
         }
 
-        // Ruch myszy – efekt hover
+        // Ruch myszy â€“ efekt hover
         else if (ev.type == ALLEGRO_EVENT_MOUSE_AXES) {
             if (ev.mouse.x >= ResolutionButton_x && ev.mouse.x <= ResolutionButton_x + ResolutionButton_w &&
                 ev.mouse.y >= ResolutionButton_y && ev.mouse.y <= ResolutionButton_y + ResolutionButton_h) {
@@ -82,12 +84,12 @@ int main() {
         else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
             if (ev.mouse.x >= ResolutionButton_x && ev.mouse.x <= ResolutionButton_x + ResolutionButton_w &&
                 ev.mouse.y >= ResolutionButton_y && ev.mouse.y <= ResolutionButton_y + ResolutionButton_h) {
-               
+
                 if (current_w == 800 && current_h == 640) {
                     al_resize_display(display, 1024, 768);
 
                     WorkspacePlace_w = 600, WorkspacePlace_h = 400;
-                    
+
                     current_w = 1024;
                     current_h = 768;
                 }
@@ -126,7 +128,7 @@ int main() {
                 int new_w = al_get_display_width(display);
                 int new_h = al_get_display_height(display);
 
-                // ponowne wyœrodkowanie przycisku
+                // ponowne wyÅ›rodkowanie przycisku
                 ResolutionButton_x = 0;
                 ResolutionButton_y = 0;
 
@@ -134,17 +136,27 @@ int main() {
                 WorkspacePlace_y = (current_h / 2.0f) - (WorkspacePlace_h / 2.0f);
             }
         }
-        // Naciœniêcie ESC — zamkniêcie
-        else if (ev.type == ALLEGRO_EVENT_KEY_DOWN && ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
-            running = false;
+        // NaciÅ›niÄ™cie ESC â€” zamkniÄ™cie
+        else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+            switch (ev.keyboard.keycode) {
+            case ALLEGRO_KEY_ESCAPE:
+                running = false;
+                break;
+
+            case ALLEGRO_KEY_F: // F â€“ przeÅ‚Ä…cza peÅ‚ny ekran
+                fullscreen = !fullscreen;
+                al_toggle_display_flag(display, ALLEGRO_FULLSCREEN_WINDOW, fullscreen);
+                break;
+            }
+        }
 
         // Rysowanie
         al_clear_to_color(al_map_rgb(30, 30, 30));
         ALLEGRO_COLOR color = hovered ? al_map_rgb(0, 120, 255) : al_map_rgb(0, 0, 0);
         al_draw_filled_rectangle(ResolutionButton_x, ResolutionButton_y, ResolutionButton_x + ResolutionButton_w, ResolutionButton_y + ResolutionButton_h, color);
-        al_draw_filled_rectangle(WorkspacePlace_x, WorkspacePlace_y, WorkspacePlace_x + WorkspacePlace_w, WorkspacePlace_y + WorkspacePlace_h, al_map_rgb(190,190,190));
+        al_draw_filled_rectangle(WorkspacePlace_x, WorkspacePlace_y, WorkspacePlace_x + WorkspacePlace_w, WorkspacePlace_y + WorkspacePlace_h, al_map_rgb(190, 190, 190));
         al_draw_rectangle(ResolutionButton_x, ResolutionButton_y, ResolutionButton_x + ResolutionButton_w, ResolutionButton_y + ResolutionButton_h, al_map_rgb(255, 255, 255), 2);
-        al_draw_text(Rozdzielczosc, al_map_rgb(255, 255, 255), ResolutionButton_x + (ResolutionButton_w/2.0), ResolutionButton_y + (ResolutionButton_h/5), ALLEGRO_ALIGN_CENTRE, "ROZDZIELCZOSC");
+        al_draw_text(Rozdzielczosc, al_map_rgb(255, 255, 255), ResolutionButton_x + (ResolutionButton_w / 2.0), ResolutionButton_y + (ResolutionButton_h / 5), ALLEGRO_ALIGN_CENTRE, "ROZDZIELCZOSC");
         al_flip_display();
     }
 
